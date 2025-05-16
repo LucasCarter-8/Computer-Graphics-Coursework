a0863ff536676f46b67942d1c5c982ca94670647
LucasCarter-8 <23636298@stu.mmu.ac.uk>
2025-05-16 11:31:28 +0100

    Self Assessment

--- source/coursework.cpp
+++ source/coursework.cpp
@@ -440,18 +440,7 @@ int main(void)
         if (data)
         {
             stbi_set_flip_vertically_on_load(false);
-            glTexImage2D
-            (
-                GL_TEXTURE_CUBE_MAP_POSITIVE_X + i,
-                0,
-                GL_RGB,
-                width,
-                height,
-                0,
-                GL_RGB,
-                GL_UNSIGNED_BYTE,
-                data
-            );
+            glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
             stbi_image_free(data);
         }
         else
